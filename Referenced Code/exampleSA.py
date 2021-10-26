c43471844151c1a7bf3b99ca3732c3999867849b
amycweng <90800337+amycweng@users.noreply.github.com>
2021-10-26 17:21:00 -0400

    copy of full code

--- Referenced Code/exampleSA.py
+++ Referenced Code/exampleSA.py
@@ -9,11 +9,88 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import seaborn as sns
 import matplotlib.pyplot
 
-dir = "..."
+dir = "/Users/shubaprasadh/Downloads/"
 dir1 = dir + "ethics_csv"
+
 windowdata_fp = dir + "topicmodel_sent"
 
+new_words = {
+    'sin': -1,
+    'whore': -1,
+    'obedience': 1,
+    'obey': 1,
+    'disobey': -1,
+    'greed': -1,
+    'hell': -1,
+    'indulgent': -0.5,
+    'indulge': -0.5,
+    'fellowship': 1,
+    'eve': -1,
+    'delilah': -1,
+    'bathsheba': -1,
+    'tempt': -1,
+    'temptation': -1,
+    'blasphemy': -1,
+    'moral': 1,
+    'natural': 1,
+    'unnatural': -1,
+    'holiness': 1,
+    'almighty': 1,
+    'lust': -1,
+    'covetousness': -1,
+    'covet': -1,
+    'deceit': -1,
+    'deceitful': -1,
+    'deceitfulness': -1,
+    'cheat': -1,
+    'wicked': -1,
+    'devil': -1,
+    'demon': -1,
+    'lucifer': -1,
+    'excess': -1,
+    'avarice': -1,
+    'green': 1,
+    'vanity': -1,
+    'pride': -1,
+    'sloth': -1,
+    'gluttony': -1,
+    'drunk': -1,
+    'drunkenness': -1,
+    'faith': 1,
+    'moral ': 1,
+    'immoral ': -1,
+    'immorality': -1,
+    'kind': 1,
+    'kinde': 1,
+    'good': 1,
+    'happiness': 1,
+    'enlightenment': 1,
+    'community': 1,
+    'dreams': 1,
+    'free': 1,
+    'friend ': -0.5,
+    'friendship': 1,
+    'scholastic': 1,
+    'ethics': 1,
+    'thought ': 1,
+    'teaching': 1,
+    'law': 1,
+    'legal': 1,
+    'justice': 1,
+    'criminal': -1,
+    'indigent': -0.5,
+    'uneducated ': -1,
+    'nobles': -0.5,
+    'nobility': 1,
+    'liberalism* ': 1,
+    'despotism': -1,
+    'gentle': 1,
+    'gentleman': 1,
+    'gentlewoman': 1
+}
+
 SIA = SentimentIntensityAnalyzer()
+SIA.lexicon.update(new_words)
 
 # THIS METHOD analyzes sentiment using vader
 def sentiment_scores(sentence):
@@ -42,7 +119,7 @@ def sentiment_scores(sentence):
 #         df.to_csv(fullname, index=False)
 
 #          Building visualizations
-data = pd.read_csv("...file path...", encoding='latin-1')
+data = pd.read_csv("/Users/shubaprasadh/Downloads/topicmodel_sent/combined_file.csv", encoding='latin-1')
 df = pd.DataFrame(data)
 df.columns = ["item","ratio","window","sent_scores"]
 df[["ratio", "sent_scores"]] = df[["ratio", "sent_scores"]].apply(pd.to_numeric)
